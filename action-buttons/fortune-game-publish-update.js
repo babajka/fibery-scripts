af873c9e5059c4de8f558d54d2c894da92622041
Ulad Bohdan <vlbohdan@gmail.com>
2020-12-25 03:07:53 +0300

    script to publish or update fortune game

---
+++ action-buttons/fortune-game-publish-update.js
@@ -0,0 +1,63 @@
+const fibery = context.getService('fibery');
+const http = context.getService('http');
+const utils = context.getService('utils');
+
+const API_URL = 'https://api.wir.by/api';
+const FRONTEND_NAME = 'dev.wir.by';
+const FRONTEND_URL = 'https://' + FRONTEND_NAME;
+const LOG_FIELD = 'Publication Log DEV'
+// TODO: Add auth token below to act as bot user.
+const AUTH_TOKEN = '';
+
+const ROLES_ALLOWED = ['Editor', 'Developer'];
+
+const thisArticle = args.currentEntities[0];
+const user = args.currentUser;
+
+const userAllowed = async () => {
+    const u = await fibery.getEntityById('fibery/user', user['Id'], ['Roles']);
+    return u['Roles'].some(role => ROLES_ALLOWED.some(roleAllowed => role['Name'].includes(roleAllowed)));
+}
+
+if (!await userAllowed()) {
+    return "❌ У вас няма правоў на выкананне дзеяння."
+}
+
+const messageBoxSecret = thisArticle[LOG_FIELD].Secret;
+
+const setLog = async (message) => {
+    return fibery.setDocumentContent(messageBoxSecret, message, 'md').catch((err) => {
+        console.log('failed to set message box:', message, err);
+    });
+}
+
+const timeNow = () => {
+    const now = new Date();
+    now.setHours(now.getHours() + 3);
+    return now.toLocaleString();
+}
+
+const logPrefix = () => `[${user['Name']}](${utils.getEntityUrl('fibery/user', user['Public Id'])}), ${timeNow()}`;
+
+await http.postAsync(`${API_URL}/games/fortune/fibery/import`, {
+    body: {
+        'fiberyPublicId': thisArticle['Public Id']
+    },
+    headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${AUTH_TOKEN}`,
+    }
+}).then(resp => {
+    const successMessage = `✅ ${logPrefix()} :  паспяхова апублікавалі гульню з fibery на [${FRONTEND_NAME}](${FRONTEND_URL}). Даступная па спасылцы: [${FRONTEND_NAME}/game/${thisArticle['Slug']}](${FRONTEND_URL}/game/${thisArticle['Slug']})`;
+    return setLog(successMessage);
+}).catch(({ innerError: { message, body } }) => {
+    const errors = JSON.parse(body);
+    const errorMessage = `❌ ${logPrefix()} :  памылка пры публікацыі гульні з fibery на [${FRONTEND_NAME}](${FRONTEND_URL}):
+        ${message}
+\`\`\`
+${JSON.stringify(errors, null, 2)}
+\`\`\``
+    return setLog(errorMessage);
+})
+
+return `Дзеянне выкананае 🌀 Праверце паспяховасць у полі "${LOG_FIELD}"`;
